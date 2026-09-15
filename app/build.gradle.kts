f1b0ee6be800e2cb239e9843fc68e6021c252913
shaobailee <127266162+ShaobaiLee@users.noreply.github.com>
2026-09-15 21:16:39 +0800

    build(gradle): bump version to 1.6 (versionCode 6002)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.cusc.media"
         minSdk = 28
         targetSdk = 34
-        versionCode = 6001
-        versionName = "1.5"
+        versionCode = 6002
+        versionName = "1.6"
         resConfigs("en")
     }
 
